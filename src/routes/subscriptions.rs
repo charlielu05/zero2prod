cbe165f2c1ada844ba315bc4d58d94b5561e868b
Charlie <charlielu05@gmail.com>
2024-05-19 05:15:38 +0000

    wip

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,8 @@ use sqlx::{query, PgPool};
 use uuid::Uuid;
 use tracing;
 
+use crate::domain::{NewSubsciber, SubscriberName};
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
@@ -15,7 +17,10 @@ pub struct FormData {
     fields(subscriber_email = %form.email, subscriber_name=%form.name))]
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form)
+    let new_subscriber = NewSubsciber {email : form.0.email, 
+        name : SubscriberName::parse(form.0.name).expect("Name validation failed")};
+
+    match insert_subscriber(&pool, &new_subscriber)
     .await
     {
         Ok(_) => {
@@ -27,15 +32,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     }
 }
 
-#[tracing::instrument(name="Saving new subscriber details in database",skip(form,pool))]
+#[tracing::instrument(name="Saving new subscriber details in database",skip(new_subscriber,pool))]
 
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData)-> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubsciber)-> Result<(), sqlx::Error> {
     sqlx::query!(r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
